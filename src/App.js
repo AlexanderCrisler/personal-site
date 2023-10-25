c36b6d89862b2e1768d1da8c39bf55f100c03ff1
Alexander Crisler <alexcrisler@gmail.com>
2023-10-24 19:58:59 -0700

    Add basic info and component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Home () {
+  return (
+    <div className="Home">
+      <h1>Alexander Crisler</h1>
+      <p>Software Engineer</p>
+      <h2>Experience</h2>
+      <h3>Employment History:</h3>
+      <ul>
+        <li>
+          <p>DTN</p>
+          <p>[timeframe]</p>
+          <p>[description]</p>
+        </li>
+        <li>
+          <p>Warpspeed</p>
+          <p>[timeframe]</p>
+          <p>[description]</p>
+        </li>
+        <li>
+          <p>Netapp</p>
+          <p>[timeframe]</p>
+          <p>[description]</p>
+        </li>
+      </ul>
+      <h3>Tools & Languages:</h3>
+      <ul>
+        <li>Python</li>
+        <li>C/C++</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>Unity</li>
+      </ul>
+      <h3>Projects:</h3>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
+      <Home />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
